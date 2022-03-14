aa8cdf0d69c82df0cf11d6d12a7747e8ff38a8ed
yannickbrocart <mail@yannickbrocart.com>
2022-03-14 12:23:15 +0100

    MAJ fin de Sprint n°1

--- src/main/java/encheres/servlets/DeconnexionServlet.java
+++ src/main/java/encheres/servlets/DeconnexionServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,15 @@ public class DeconnexionServlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		if (session != null)
 			session.invalidate();
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("pseudoUtilisateur")) {
+					cookie.setMaxAge(0);
+					response.addCookie(cookie);
+				}
+			}
+		}
 		request.getRequestDispatcher("/").forward(request, response);
 	}
 
